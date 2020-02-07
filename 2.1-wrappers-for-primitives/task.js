bfa1c963203a851ddaca9a57cb9a617ba1cd8e34
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-02-07 13:47:17 +0300

    Old tasks re-uploaded

--- 2.1-wrappers-for-primitives/task.js
+++ 2.1-wrappers-for-primitives/task.js
@@ -1,9 +1,59 @@
+function calculateMortgage() {
+    let percent = window.percent.value;
+    let contribution = window.contribution.value;
+    let amount = window.amount.value;
+    let date = window.date.value;
+
+    let result = calculateTotalMortgage(percent, contribution, amount, date);
+    let span = window.mortageResult;
+    span.textContent = result;
+}
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+
+    'use strict';
+
+    function isNumber(item) {
+        if (Number.isFinite(Number(item))) {
+            return item
+        } else {
+            console.log(`Параметр содержит неправильное значение ${item}`);
+        }
+    }
+
+    isNumber(percent);
+    isNumber(contribution);
+    isNumber(amount);
+    isNumber(date);
+
+    let today = +new Date();
+    let payDate = +new Date(date);
+    let months = (payDate - today) / (1000 * 60 * 60 * 24 * 30);
+
+    let monthPrcnt = percent / 1200;
+    let payMonth = (amount - contribution) * (monthPrcnt + monthPrcnt / (((1 + monthPrcnt) ** 12) - 1));
+    let totalAmount = (payMonth * months).toFixed(2);
+    return totalAmount;
+}
+calculateTotalMortgage(10, 0, 50000, 12)
+
+function sayHello() {
+    let name = window.personName.value;
+    let greeting = getGreeting(name);
+    let span = window.helloResult;
+    span.textContent = greeting;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    if (name === null || name === "" || name === undefined) {
+        name = 'Аноним'
+    }
+    return `Привет, мир! Меня зовут ${name}.`;
 }
+
+console.log(getGreeting('Василий'));
+console.log(getGreeting('Евгений'));
+console.log(getGreeting('Сергей'));
+console.log(getGreeting(null));
+console.log(getGreeting(""));
+console.log(getGreeting(undefined));
